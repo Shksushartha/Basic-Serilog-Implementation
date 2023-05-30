936f0d801fb910dd9e8ba4b7267f53e6c545a8d0
Shksushartha <shksushartha@gmail.com>
2023-05-30 12:52:56 +0545

    initial commit

---
+++ serilogDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace serilogDemo.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        _logger.LogInformation("this is the log");
+        try
+        {
+            for(int i = 0; i<50; i++)
+            {
+                if(i == 25)
+                {
+                    throw new Exception("This is the 25 error");
+                }
+                else
+                {
+                    _logger.LogInformation("The value of i is {i}", i);
+                }
+
+            }
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError(ex,"the error is catched");
+        }
+
+    }
+}
+
---
+++ serilogDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿using Serilog;
+
+namespace serilogDemo;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       
+
+        try
+        {
+            
+            var builder = WebApplication.CreateBuilder(args);
+
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var Logger = new LoggerConfiguration()
+               .ReadFrom.Configuration(configuration)
+               .CreateLogger();
+
+            builder.Host.UseSerilog(Logger);
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+            Logger.Information("application starting up.");
+            // Configure the HTTP request pipeline.
+            //if (!app.Environment.IsDevelopment())
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
+            app.UseSerilogRequestLogging();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+
+           
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+        catch(Exception ex)
+        {
+            Log.Fatal(ex,"App starting failed");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+        
+    }
+
+   
+}
